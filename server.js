cfdd01ba43b400543a2d0809075605588ecaeeb1
Mustafa Makarnacı <mustafazymakarnaci@gmail.com>
2021-04-04 04:06:45 +0300

    prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');//bring express
 const connectDB = require('./config/db')
-
+const path = require('path')
 const app = express();//initialize app variable
 
 // Connect Database
@@ -8,15 +8,24 @@ const app = express();//initialize app variable
 connectDB();
 
 //Init Middleware
-app.use(express.json({ extented: false}));
-
-app.get('/', (req, res) => res.send('API Running'))
+app.use(express.json({ extented: false }));
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
